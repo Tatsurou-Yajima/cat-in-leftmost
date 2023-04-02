a6827cf87d5279ea2cf78ff542680f921d9fd513
T.Yajima <tatsurou.yajima@gmail.com>
2023-04-02 18:44:48 +0900

    :tada: クリック時に尻尾が動くようにした

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ const init = async () => {
 
     div.appendChild(catImage);
     body.appendChild(div);
+
+    catImage.addEventListener("click", function () {
+        catImage.src = chrome.runtime.getURL("images/black_cat.gif");
+
+        setTimeout(function () {
+            catImage.src = chrome.runtime.getURL("images/black_cat.png");
+        }, 3000);
+    });
+
 };
 
 init();
